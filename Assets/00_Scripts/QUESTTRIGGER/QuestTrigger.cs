2d5f4b49e99bba467d8d3f998df49d4e9d1af245
Sylve <sylve.renault@yahoo.fr>
2022-09-25 15:58:39 +0200

    gyuu

--- Assets/00_Scripts/QUESTTRIGGER/QuestTrigger.cs
+++ Assets/00_Scripts/QUESTTRIGGER/QuestTrigger.cs
@@ -13,6 +13,7 @@ public class QuestTrigger : MonoBehaviour
     void Awake()
     {
         gManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        UIScore = GameObject.FindGameObjectWithTag("UIScore");
     }
     // Start is called before the first frame update
     void Start()
